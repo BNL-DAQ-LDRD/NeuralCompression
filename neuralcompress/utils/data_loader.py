effa3fd49eb130adcf3c3266125551ee457fdc0b
Yi Huang <yhuang2@wavelet.csi.bnl.gov>
2021-11-14 22:29:18 -0500

    data_loader that is (almost) dataset independent

--- neuralcompress/utils/data_loader.py
+++ neuralcompress/utils/data_loader.py
@@ -1,62 +1,120 @@
+"""
+Goal:
+    Given a dataset API and a split filename, get one or more dataloaders.
+"""
+
 #! /usr/bin/env python
-import sys
-from torch.utils.data import DataLoader
-
-sys.path.append('/home/yhuang2/PROJs/NeuralCompression/neuralcompress/')
-
-from datasets.tpc_dataset import DatasetTPC3d
-from utils.data_splitter import Splitter
+import torch
+from torch.utils.data import DataLoader, Subset
 
 
-def get_dataloaders(
-    data_path,
-    shuffle=True,
-    rng_random_state=0,
-    max_sizes=None,
-    batch_size=32
+def get_dataloader(
+    dataset_api,
+    split_fname,
+    batch_size,
+    lengths=None,
+    seed=None
 ):
-    split_path = data_path
+    """
+    Take a dataset_api that only have split file return data loaders.
+    Input:
+        - dataset_api (Dataset API): a dataset api that takes only one input
+            which is the split file that contains the list of
+            symlinks to the datafile;
+        - split_fname (str): The filename of the split file;
+        - batch_size (int): batch_size of the data loader;
+        - lengths (int or list(int)): if lengths is an integer,
+            return a data loader of containing lengths many data points;
+            If lengths is a list of integers, return a list of data loaders
+            of length len(lengths) where data loaders[i] contains length[i]
+            data points;
+        - seed (int): If seed is given randomly shuffle the datafiles
+    Output:
+        - a DataLoader object or a list of DataLoader object.
+    """
 
-    splitter = Splitter(
-        data_path,
-        split_path,
-        max_sizes=max_sizes,
-        shuffle=shuffle,
-        rng_random_state=rng_random_state
-    )
+    # get dataset
+    dataset = dataset_api(split_fname)
 
-    splitter.split()
+    # If length is not given return the data loader
+    if not lengths:
+        return DataLoader(dataset, batch_size=batch_size)
 
-    dataset_train = DatasetTPC3d(split_path, data_path, split='train')
-    dataset_valid = DatasetTPC3d(split_path, data_path, split='valid')
-    dataset_test  = DatasetTPC3d(split_path, data_path, split='test' )
+    # If length is given, parse length, and check validity
+    if isinstance(lengths, int):
+        total_length = lengths
+    else:
+        total_length = sum(lengths)
+    assert total_length <= len(dataset), 'sum(lengths) > len(dataset)'
 
-    loader_train = DataLoader(dataset_train, batch_size=batch_size)
-    loader_valid = DataLoader(dataset_valid, batch_size=batch_size)
-    loader_test  = DataLoader(dataset_test,  batch_size=batch_size)
+    # get indices
+    # If seed is given, random shuffle the indices
+    if seed is not None:
+        indices = torch.randperm(
+            len(dataset),
+            generator=torch.Generator().manual_seed(seed)
+        )
+        indices = indices[:total_length]
+    else:
+        indices = torch.arange(0, total_length)
 
-    loaders = [loader_train, loader_valid, loader_test]
-    return loaders
+    # if lengths is just one integer, return the dataloader
+    if isinstance(lengths, int):
+        return DataLoader(
+            Subset(dataset, indices),
+            batch_size=batch_size
+        )
+    # if lengths is a sequence, return a sequence of datalaoder
+    data_loaders = []
+    start = 0
+    for length in lengths:
+        end = start + length
+        loader = DataLoader(
+            Subset(dataset, indices[start: end]),
+            batch_size=batch_size
+        )
+        data_loaders.append(loader)
+        start = end
+    return data_loaders
+
+
+# def test():
+#     """
+#     Test with TPC dataset API.
+#     """
+#     import sys
+#     sys.path.append('/home/yhuang2/PROJs/NeuralCompression/neuralcompress/')
+#     from pathlib import Path
+#     from datasets.tpc_dataset import DatasetTPC3d
+#
+#     batch_size = 32
+#     root_folder = Path('/data/datasets/sphenix/highest_framedata_3d/outer/')
+#     train_loader, valid_loader = get_dataloader(
+#         DatasetTPC3d,
+#         root_folder/'train.txt',
+#         batch_size,
+#         lengths=[300, 100],
+#         seed=0
+#     )
+#
+#     print('\ntrain')
+#     for i, batch in enumerate(train_loader):
+#         print(f'{i + 1}/{len(train_loader)}: {len(batch)}')
+#     print('\nvalid')
+#     for i, batch in enumerate(valid_loader):
+#         print(f'{i + 1}/{len(valid_loader)}: {len(batch)}')
+#
+#     loader = get_dataloader(
+#         DatasetTPC3d,
+#         root_folder/'test.txt',
+#         batch_size=32,
+#         lengths=200
+#     )
+#     print('\ntest')
+#     for i, batch in enumerate(loader):
+#         print(f'{i + 1}/{len(loader)}: {len(batch)}')
 
 
 if __name__ == "__main__":
     print('This is main of data_loader.py')
-
-    # frame_path = '/data/datasets/sphenix/highest_framedata_3d/outer/'
-    # max_sizes = [300, 100, 100]
-    # shuffle = True
-    # rng_random_state = 0
-    # batch_size = 32
-
-    # loaders = get_dataloaders(
-    #     frame_path,
-    #     shuffle=shuffle,
-    #     rng_random_state=rng_random_state,
-    #     max_sizes=max_sizes,
-    #     batch_size=batch_size
-    # )
-
-    # for loader in loaders:
-    #     for i, batch in enumerate(loader):
-    #         print(f'{i + 1}/{len(loader)}: {len(batch)}')
-    #     print()
+    # test()
