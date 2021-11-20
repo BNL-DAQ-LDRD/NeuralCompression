ee3bb90787254df6d3e2919310ec500c5f389be5
Yi Huang <yhuang2@wavelet.csi.bnl.gov>
2021-11-19 21:37:45 -0500

    new data loader

--- neuralcompress/utils/tpc_dataloader.py
+++ neuralcompress/utils/tpc_dataloader.py
@@ -3,15 +3,42 @@ Get TPC train, valid, and test dataloaders
 """
 #! /usr/bin/env python
 from pathlib import Path
-from torch.utils.data import DataLoader
 from neuralcompress.datasets.tpc_dataset import DatasetTPC3d
-from neuralcompress.utils.dataset_utils import (
-    sample_dataset,
-    split_dataset
+import torch
+from torch.utils.data import (
+    RandomSampler,
+    random_split,
+    DataLoader
 )
 
+def subsample_dataset(
+    dataset,
+    sample_sz = None,
+    shuffle   = True,
+    seed      = None
+):
+    """
+    subsample a dataset
+    """
+    assert 0 <= sample_sz <= len(dataset), \
+        f'dataset does not contains test_sz ({sample_sz}) many examples'
+
+    if shuffle:
+        gen = None if seed is None else torch.Generator().manual_seed(seed)
+        dataset = RandomSampler(
+            dataset,
+            replacement = False,
+            num_samples = sample_sz,
+            generator   = gen
+        )
+    else:
+        dataset = dataset[:sample_sz]
+
+    return dataset
+
+
 def get_tpc_test_dataloader(
-    test_manifest,
+    manifest,
     batch_size,
     test_sz=None,
     shuffle=True,
@@ -20,8 +47,13 @@ def get_tpc_test_dataloader(
     """
     Get TPC test dataloader
     """
-    dataset = DatasetTPC3d(test_manifest)
-    dataset = sample_dataset(dataset, test_sz, shuffle, seed)
+    dataset = subsample_dataset(
+        DatasetTPC3d(manifest),
+        sample_sz = test_sz,
+        shuffle   = shuffle,
+        seed      = seed
+    )
+
     return DataLoader(dataset, batch_size=batch_size)
 
 # pylint: disable=too-many-arguments
@@ -44,21 +76,27 @@ def get_tpc_train_valid_dataloaders(
     ), 'give train size and valid size or just valid ratio'
 
     dataset = DatasetTPC3d(train_manifest)
-
     if train_sz is not None:
-        train_dataset, valid_dataset = split_dataset(
+        dataset = subsample_dataset(
             dataset,
-            sizes=[train_sz, valid_sz],
-            shuffle=shuffle,
-            seed=seed
+            sample_sz = train_sz + valid_sz,
+            shuffle   = shuffle,
+            seed      = seed
         )
     else:
-        train_dataset, valid_dataset = split_dataset(
+        train_sz = int(len(dataset) / (1 + valid_ratio))
+        valid_sz = len(dataset) - train_sz
+
+    if shuffle:
+        gen = None if seed is None else torch.Generator().manual_seed(seed)
+        train_dataset, valid_dataset = random_split(
             dataset,
-            fractions=[1, valid_ratio],
-            shuffle=shuffle,
-            seed=seed
+            lengths   = [train_sz, valid_sz],
+            generator = gen
         )
+    else:
+        train_dataset = dataset[:train_sz]
+        valid_dataset = dataset[train_sz:]
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size)
     valid_loader = DataLoader(valid_dataset, batch_size=batch_size)
