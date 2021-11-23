ea78e550f8f006b4e4f8d5088e5a13843e7f78c8
Yi Huang <yhuang2@wavelet.csi.bnl.gov>
2021-11-23 10:55:21 -0500

    update test tpc_block to include a dummy decoder

--- test/test_tpc_blocks.py
+++ test/test_tpc_blocks.py
@@ -1,7 +1,6 @@
 """
 Test the `neuralcompress/models/blocks.py`
 """
-import argparse
 from neuralcompress.models.blocks import (
     encoder_residual_block,
     decoder_residual_block
@@ -36,12 +35,48 @@ class Encoder(nn.Module):
     def forward(self, x_input):
         return self.layers(x_input)
 
+class Decoder(nn.Module):
+    """
+    A dummy encoder
+    """
+    def __init__(
+        self,
+        num_layers,
+        activ=None,
+        norm=None
+    ):
+        super().__init__()
+        deconv_args = {
+            'in_channels': 8,
+            'out_channels': 8,
+            'kernel_size': 4,
+            'stride': 2,
+            'padding': 1
+        }
+        layers = []
+        for _ in range(num_layers):
+            layer = decoder_residual_block(deconv_args, activ, norm)
+            layers.append(layer)
+        self.layers = nn.Sequential(*layers)
 
-encoder = Encoder(3, 'relu', 'batch')
-tensor_in = torch.randn(32, 8, 16, 16, 16)
-tensor_out = encoder(tensor_in)
-print(tensor_out.shape)
+    def forward(self, x_input):
+        return self.layers(x_input)
 
-scripted = torch.jit.script(encoder)
-scripted.save('checkpoints/test_tpc_blocks/encoder.pt')
+if __name__ == '__main__':
 
+    encoder = Encoder(3, 'relu', 'batch')
+    tensor_in = torch.randn(32, 8, 16, 16, 16)
+    tensor_out = encoder(tensor_in)
+    print(tensor_out.shape)
+
+    scripted = torch.jit.script(encoder)
+    scripted.save('checkpoints/test_tpc_blocks/encoder.pt')
+
+
+    encoder = Decoder(3, 'relu', 'batch')
+    tensor_in = tensor_out
+    tensor_out = encoder(tensor_in)
+    print(tensor_out.shape)
+
+    scripted = torch.jit.script(encoder)
+    scripted.save('checkpoints/test_tpc_blocks/decoder.pt')
