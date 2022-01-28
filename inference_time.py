adeaa96d6302642374b3a370dcdfeec233026f0d
Yi Huang <yhuang2@wavelet.csi.bnl.gov>
2022-01-28 00:03:58 -0500

    inference time script

--- inference_time.py
+++ inference_time.py
@@ -1,35 +1,30 @@
 """
 Inference time study
 """
+import argparse
 from pathlib import Path
-import tqdm
+from time import time
+import pandas as pd
 import numpy as np
-from neuralcompress.utils.load_bcae_models import load_bcae_encoder
-from neuralcompress.utils.tpc_dataloader import get_tpc_dataloaders
+
 import torch
 import torch.backends
-cudnn.benchmark = True
 
-# dataloader number of workers and pin_memory
-# pre-fetching = 1 or 2
+from neuralcompress.utils.load_bcae_models import load_bcae_encoder
+from neuralcompress.utils.tpc_dataloader import get_tpc_dataloaders
 
 
 DATA_ROOT = '/data/datasets/sphenix/highest_framedata_3d/outer'
+CHECKPOINTS = '/home/yhuang2/PROJs/NeuralCompression_results/checkpoints/'
+EPOCH = 440
 
 def inference():
+    """
+    inference time study
+    """
 
     parser = argparse.ArgumentParser(
         description="Run BCAE inference"
-    )
-
-
-    parser.add_argument(
-        '--device',
-        required = False,
-        default  = 'cuda',
-        choices  = ['cuda', 'cpu'],
-        type     = str,
-        help     = "The device to run the inference."
     )
 
     parser.add_argument(
@@ -37,145 +32,107 @@ def inference():
         required = False,
         default  = 1,
         type     = int,
-        help     = "Number of frames to load | default=1."
+        help     = "Number of frames to load. (default=1)"
     )
-
     parser.add_argument(
         '--batch_size',
         required = False,
         default  = 1,
         type     = int,
-        help     = "Batch size | default=1."
+        help     = "Batch size. (default=1)"
     )
-
     parser.add_argument(
-        '--partition',
+        '--num_runs',
         required = False,
-        default  = 'test',
-        choices=['train', 'valid', 'test'],
-        type     = str,
-        help     = "partition from which to load the data | default=test."
+        default  = 10,
+        type     = int,
+        help     = "Number of runs to calculate the run-time. (default=10)"
     )
-
     parser.add_argument(
-        '--random',
-        action = 'store_true',
-        help   = "Whether to get a random sample."
+        # If True, the data loader will copy Tensors
+        # into CUDA pinned memory before returning them.
+        '--pin_memory',
+        action   = 'store_true',
+        help     = "If True, the dataloader will copy Tensors into CUDA \
+                    pinned memory before returning them."
     )
-
     parser.add_argument(
-        '--checkpoint_path',
-        required=True,
-        type=str,
-        help="The path to the checkpoints."
-    )
-
-    parser.add_argument(
-        '--epoch',
-        required=True,
-        type=int,
-        help="The epoch to load."
-    )
-
-    parser.add_argument(
-        '--save_path',
-        required = True,
-        type     = str,
-        help     = "The path to save output tensor."
-    )
-
-    parser.add_argument(
-        '--half',
-        action = 'store_true',
-        help   = "Whether to save the output with half precision."
-    )
-
-
-    parser.add_argument(
-        '--prefix',
+        '--num_workers',
         required = False,
-        default  = 'output',
-        type     = str,
-        help     = "Output file prefix."
+        default  = 0,
+        type     = int,
+        help     = "how many subprocesses to use for data loading. \
+                    0 means that the data will be loaded in the main process. \
+                    (default=0)"
+    )
+    parser.add_argument(
+        '--prefetch_factor',
+        required = False,
+        default  = 2,
+        type     = int,
+        help     = "Number of samples loaded in advance by each worker. \
+                    (default=2)"
+    )
+    parser.add_argument(
+        '--benchmark',
+        action   = 'store_true',
+        help     = "If True, set torch.backends.cudnn.benchmark to True."
     )
 
     args = parser.parse_args()
 
+    torch.backends.cudnn.benchmark = False
 
     # Load data
     data_config = {
-        'batch_size' : args.batch_size,
-        'train_sz'   : 0,
-        'valid_sz'   : 0,
-        'test_sz'    : 0,
-        'is_random'  : args.random,
+        'batch_size'      : args.batch_size,
+        'train_sz'        : args.data_size,
+        'valid_sz'        : 0,
+        'test_sz'         : 0,
+        'is_random'       : True,
+        'pin_memory'      : args.pin_memory,
+        'num_workers'     : args.num_workers,
+        'prefetch_factor' : args.prefetch_factor
     }
-    data_config[f'{args.partition}_sz'] = args.data_size
 
-    data_path = Path(args.data_path)
-    assert data_path.exists(), f'{data_path} does not exist!'
-
-    loaders = get_tpc_dataloaders(data_path, **data_config)
-    partition = args.partition
-    if partition == 'train':
-        loader = loaders[0]
-    elif partition == 'valid':
-        loader = loaders[1]
-    else:
-        loader = loaders[2]
-
+    data_path = Path(DATA_ROOT)
+    loader, _, _ = get_tpc_dataloaders(data_path, **data_config)
 
     # Load encoder
-    checkpoint_path = Path(args.checkpoint_path)
-    assert checkpoint_path.exists(), f'{checkpoint_path} does not exist!'
+    encoder = load_bcae_encoder(CHECKPOINTS, EPOCH)
+    encoder.to('cuda')
 
-    encoder = load_bcae_encoder(checkpoint_path, args.epoch)
-    encoder.to(args.device)
-
-    # run inference
-    # progbar = tqdm.tqdm(
-    #     desc="BCAE Inference",
-    #     total=len(loader),
-    #     dynamic_ncols=True
-    # )
-    # outputs = []
-
-    res = {}
-
+    res = vars(args)
+    num_runs = args.num_runs
+    mb = 1024 ** 2
+    records = []
     with torch.no_grad():
-        for i, batch in enumerate(loader):
-            output = encoder(batch.to(args.device))
+        # Run one epoch before timing so that cuda can adjust well.
+        for batch in loader:
+            _ = encoder(batch.to('cuda'))
 
-        res['    memory allocated (MB)'.strip()] = torch.cuda.memory_allocated()/1024/1024
-        res['    memory cached    (MB)'.strip()] = torch.cuda.memory_cached()/1024/1024
-        res['max memory allocated (MB)'.strip()] = torch.cuda.max_memory_allocated()/1024/1024
-        res['max memory cached    (MB)'.strip()] = torch.cuda.max_memory_cached()/1024/1024
-
+        res['    memory allocated (MB)'] = torch.cuda.memory_allocated()/mb
+        res['    memory reserved  (MB)'] = torch.cuda.memory_reserved()/mb
+        res['max memory allocated (MB)'] = torch.cuda.max_memory_allocated()/mb
+        res['max memory reserved  (MB)'] = torch.cuda.max_memory_reserved()/mb
 
         time0 = time()
-        T = 10
-        for _ in range(T):
-            for i, batch in enumerate(loader):
-                output = encoder(batch.to(args.device))
-                # outputs.append(output.detach().cpu().numpy())
-                # progbar.update()
-            # progbar.close()
+        for _ in range(num_runs):
+            time_sub = time()
+            for batch in loader:
+                _ = encoder(batch.to('cuda'))
+            torch.cuda.synchronize()
+            records.append(time() - time_sub)
         torch.cuda.synchronize()
         time1 = time()
 
-    # save_path = Path(args.save_path)
-    # if not save_path.exists():
-    #     save_path.mkdir(parents=True)
+    multiplier = 100 / (args.data_size * num_runs)
+    res['inference time per hundred frames'] = (time1 - time0) * multiplier
+    # res['mean'] = np.mean(records) * multiplier * num_runs
+    res['std'] = np.std(records) * multiplier * num_runs
 
-    # # save result
-    # counter = 0
-    # for output in outputs:
-    #     for frame in output:
-    #         if args.half:
-    #             frame = frame.astype('float16')
-    #         fname = save_path/f'{args.prefix}_{counter}'
-    #         np.savez(fname, data=frame)
-    #         counter += 1
+    return res
 
-if _# _name__ == '__main__':
-    # inference()
+if __name__ == '__main__':
+    df = pd.DataFrame(data=inference(), index=[1])
+    print(df)
