59b4abacaf8df41f8776584a34c5c4124dbda5df
Yi Huang <yhuang2@wavelet.csi.bnl.gov>
2022-04-29 14:37:47 -0400

    update test.py

--- test.py
+++ test.py
@@ -15,11 +15,10 @@ from neuralcompress.utils.load_bcae_models import (
 from neuralcompress.models.bcae_combine import BCAECombine
 
 
-
+#################################################################
 # =================== Compress and decompress ===================
-
 # Load data
-data_path   = 'data'
+data_path   = Path('./data')
 data_config = {
     'batch_size' : 4,
     'train_sz'   : 0,
@@ -42,6 +41,7 @@ decoder.to(device)
 
 # run compression and decompression
 combine = BCAECombine()
+originals    = []
 compressed   = []
 decompressed = []
 progbar = tqdm.tqdm(
@@ -49,7 +49,6 @@ progbar = tqdm.tqdm(
     total=len(loader),
     dynamic_ncols=True
 )
-
 with torch.no_grad():
     for batch in loader:
         batch  = batch.to(device)
@@ -58,57 +57,75 @@ with torch.no_grad():
         comp   = comp.half() # we save the compressed result as half float
         decomp = combine(decoder(comp.float()))
 
+        originals.append(batch.detach().cpu().numpy())
         compressed.append(comp.detach().cpu().numpy())
         decompressed.append(decomp.detach().cpu().numpy())
 
         progbar.update()
     progbar.close()
 
+# reshape the tensors
+originals    = np.squeeze(np.vstack(originals))
+compressed   = np.vstack(compressed)
+decompressed = np.squeeze(np.vstack(decompressed))
+
 # save result
 save_path = Path('results')
-counter = 0
-for comp, decomp in zip(compressed, decompressed):
-    for en, de in zip(comp, decomp):
-        de = np.squeeze(de)
-        np.savez(save_path/f'compressed_{counter}', data=en)
-        np.savez(save_path/f'decompressed_{counter}', data=de)
-        counter += 1
+if not save_path.exists():
+    save_path.mkdir()
+
+for i, (comp, decomp) in enumerate(zip(compressed, decompressed)):
+    np.save(save_path/f'compressed_{i}', comp)
+    np.save(save_path/f'decompressed_{i}', decomp)
 
 
 
-#=================== Load result and compute metrices ===================
-
-print('\nCompute MSE, log-MAE, and PSNR')
-# load input
-fnames_raw = sorted(list(Path('./data').glob('*npy')))
-# load reconstructed image
-fnames_rec = sorted(list(Path('./results').glob('decomp*npz')))
-
-# metrics
-results = defaultdict(list)
-
-for i, (fname_raw, fname_rec) in enumerate(zip(fnames_raw, fnames_rec)):
-    raw = np.load(fname_raw)
-    with np.load(fname_rec) as fh:
-        rec = fh[fh.files[0]]
-
+###################################################################
+# =================== Sanity check with metrics ===================
+print('\n============== Recontruction Errors ===============')
+print('Metrics obtained from the current run:')
+metrics = defaultdict(list)
+for i, (orig, decomp) in enumerate(zip(originals, decompressed)):
     # mean squared error and peak signal-noise ratio
-    diff = np.abs(raw - rec)
+    diff = np.abs(orig - decomp)
     mse  = np.mean(diff * diff)
     psnr = np.log10(1023 ** 2 / mse)
     # Log Mean absolute error
-    log_raw  = np.log2(raw + 1)
-    log_rec  = np.log2(rec + 1)
-    log_diff = np.abs(log_raw - log_rec)
-    log_mae  = np.mean(log_diff)
+    log_orig   = np.log2(orig + 1)
+    log_decomp = np.log2(decomp + 1)
+    log_diff   = np.abs(log_orig - log_decomp)
+    log_mae    = np.mean(log_diff)
 
-    results['mse'].append(mse)
-    results['log_mae'].append(log_mae)
-    results['psnr'].append(psnr)
+    metrics['mse'].append(mse)
+    metrics['log_mae'].append(log_mae)
+    metrics['psnr'].append(psnr)
 
-    print(f'\tsample {i}: mse = {mse:.3f}, log mae = {log_mae: .3f}, psnr = {psnr: .3f}')
-
-df = pd.DataFrame(data=results)
+df = pd.DataFrame(data=metrics)
 descr = df.describe().loc[['mean', 'std'], :]
-print()
-print(descr)
+print(f'{df}\n{descr}')
+
+print('\nExpected metrics:')
+df_exp = pd.read_csv(data_path/'sample_metric_results.csv', index_col=0)
+descr_exp = df_exp.describe().loc[['mean', 'std'], :]
+print(f'{df_exp}\n{descr_exp}')
+
+
+##########################################################################
+# =================== Load result and compute metrices ===================
+
+print('\n========= Compare current output with cached ones =========')
+# load compressed and decompressed filenames
+fnames_comp   = sorted(list(Path('./data').glob('comp*npy')))
+fnames_decomp = sorted(list(Path('./data').glob('decomp*npy')))
+
+for i, (comp, fname_comp) in enumerate(zip(compressed, fnames_comp)):
+    comp_cached = np.load(fname_comp)
+    diff = np.abs(comp - comp_cached)
+    mse  = np.mean(diff * diff)
+    print(f'Sample {i} compressed: MSE = {mse:.3e}')
+
+for i, (decomp, fname_decomp) in enumerate(zip(decompressed, fnames_decomp)):
+    decomp_cached = np.load(fname_decomp)
+    diff = np.abs(decomp - decomp_cached)
+    mse  = np.mean(diff * diff)
+    print(f'Sample {i} decompressed: MSE = {mse:.3e}')
